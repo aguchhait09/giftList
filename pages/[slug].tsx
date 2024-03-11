1089eef4156970b861bed6ca52b00aa7cf7fdbf8
avik12 <avik.guchhait@webskitters.com>
2024-03-11 20:27:11 +0530

    test

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -2,19 +2,25 @@ import React from "react";
 import { allProductsApi } from "@/api/functions/products.api";
 import CustomCard from "@/components/CustomCard/CustomCard";
 import Wrapper from "@/layout/wrapper/Wrapper";
-import { Container, Grid, styled } from "@mui/material";
+import { Box, Button, Container, Grid, styled } from "@mui/material";
 import { useRouter } from "next/router";
 import { HomeProps } from "pages";
 
 const SlyedContainer = styled(Container)`
   margin-top: 25px;
 `;
+const FilterContainer = styled(Box)`
+  margin: 10px;
+`;
+const StyledButton = styled(Button)`
+  margin: 5px;
+  background: #75ced7;
+`;
 
 const Index = ({ productsData }: HomeProps) => {
   const router = useRouter();
   const { slug } = router.query;
   // console.log("slug", slug);
-  // console.log("Data", productsData);
   const data = productsData?.filter((dta) => {
     if (dta?.slug === slug) return dta;
   });
@@ -23,30 +29,45 @@ const Index = ({ productsData }: HomeProps) => {
   return (
     <Wrapper>
       <SlyedContainer>
+        <FilterContainer>
+          {productsData?.map((item) => {
+            return (
+              <>
+                {item?.slug === slug ? (
+                  <StyledButton variant="contained" disabled>
+                    {item?.title}
+                  </StyledButton>
+                ) : (
+                  <StyledButton variant="contained" onClick={()=>router.push(`/${item?.slug}`)}>{item?.title}</StyledButton>
+                )}
+              </>
+            );
+          })}
+        </FilterContainer>
         {data?.map((item) => {
           return (
-              <Grid
-                container
-                rowSpacing={1}
-                columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-              >
-                {item?.products?.map((element) => {
-                  return (
-                      <Grid item xs={3}>
-                        <CustomCard
-                          title={element?.brand_name}
-                          subtitle={`${element?.product_name?.slice(0, 18)}` + "..."}
-                          cardImage={
-                            element?.product_meta_data[3]?.value?.image
-                          }
-                          price={element?.product_regular_price}
-                          offerPrice={element?.product_sale_price}
-                          subtitleLink={element?.product_link}
-                        />
-                      </Grid>
-                  );
-                })}
-              </Grid>
+            <Grid
+              container
+              rowSpacing={1}
+              columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+            >
+              {item?.products?.map((element) => {
+                return (
+                  <Grid item xs={3}>
+                    <CustomCard
+                      title={element?.brand_name}
+                      subtitle={
+                        `${element?.product_name?.slice(0, 18)}` + "..."
+                      }
+                      cardImage={element?.product_meta_data[3]?.value?.image}
+                      price={element?.product_regular_price}
+                      offerPrice={element?.product_sale_price}
+                      subtitleLink={element?.product_link}
+                    />
+                  </Grid>
+                );
+              })}
+            </Grid>
           );
         })}
       </SlyedContainer>
@@ -58,4 +79,5 @@ export const getServerSideProps = async () => {
   const productsData = await allProductsApi();
   return { props: { productsData } };
 };
+
 export default Index;
